9a06205fe5f36dea1a1531c8c4c24f2b323f5a42
zoutigo <zoutigo@gmail.com>
2022-03-01 11:25:11 +0100

    ajout fixtures + modification entité Comment

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -63,10 +63,7 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      */
     private $articles;
 
-    /**
-     * @ORM\OneToMany(targetEntity=Comment::class, mappedBy="author", orphanRemoval=true)
-     */
-    private $comments;
+   
 
     public function __construct()
     {
@@ -241,33 +238,5 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
         return $this;
     }
 
-    /**
-     * @return Collection<int, Comment>
-     */
-    public function getComments(): Collection
-    {
-        return $this->comments;
-    }
-
-    public function addComment(Comment $comment): self
-    {
-        if (!$this->comments->contains($comment)) {
-            $this->comments[] = $comment;
-            $comment->setAuthor($this);
-        }
-
-        return $this;
-    }
-
-    public function removeComment(Comment $comment): self
-    {
-        if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
-            if ($comment->getAuthor() === $this) {
-                $comment->setAuthor(null);
-            }
-        }
-
-        return $this;
-    }
+ 
 }
